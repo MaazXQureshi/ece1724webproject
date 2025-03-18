dd59bb5f1b142f05566e5e69d59eddfa91cd7ab0
Elly Wong <ellyw1002@gmail.com>
2025-03-17 21:49:33 -0400

    initialize and create skeleton code for frontend

---
+++ frontend/src/routes/Home.tsx
@@ -0,0 +1,15 @@
+import {Button} from "@/components/ui/button.tsx";
+
+
+function Home() {
+
+    return (
+        <>
+            <div>
+                <Button variant={"outline"}>Click</Button>
+            </div>
+        </>
+    )
+}
+
+export default Home
