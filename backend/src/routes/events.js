7116b564f97f67561a433d7eb710da4278a75dd8
Elly Wong <ellyw1002@gmail.com>
2025-03-31 01:09:10 -0400

    [BACKEND] added missing dependencies to package.json

--- backend/src/routes/events.js
+++ backend/src/routes/events.js
@@ -10,12 +10,13 @@ const middleware = require("../middleware/middleware");
 // PUT /events/:id - Update an event.
 // DELETE /events/:id - Delete an event.
 // POST /events/:id/register - Register a user for an event (RSVP)
+// TODO: GET /user-events - Get all events that a user registered for
 
 router.post('/', middleware.validateEventData, controller.createEvent);
 router.get('/', middleware.validateEventQuery, controller.getAllEvents);
 router.get('/:id', middleware.validateResourceId, controller.getEventById);
 router.put('/:id', middleware.validateResourceId, middleware.validateEventData, controller.updateEvent);
-router.delete('/:id', middleware.validateResourceId,  controller.deleteEvent);
+router.delete('/:id', middleware.validateResourceId, controller.deleteEvent);
 router.post('/register', controller.respondToEvent);
 router.post('/unregister', controller.unregisterForEvent);
 
